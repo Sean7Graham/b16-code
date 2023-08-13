ca9faffeacc24ed70b51ea4fe4bac815a127481c
Andrea Vedaldi <vedaldi@gmail.com>
2023-08-13 23:20:40 +0100

    Initial.

---
+++ part-1/queue_enhanced.hpp
@@ -0,0 +1,50 @@
+#ifndef __queue_enhanced__
+#define __queue_enhanced__
+
+#include <cassert>
+#include <vector>
+
+#include "queue.hpp"
+
+template <typename T> class Dequeue : public Queue<T>
+{
+  public:
+    // Inherit the constructors of Queue<T>
+    using Queue<T>::Queue;
+
+    // Access the element at the back of the queue
+    T &back()
+    {
+        // WRITE YOUR CODE HERE
+    }
+
+    // Const-access the element at the back of the queue
+    const T &back() const
+    {
+        // WRITE YOUR CODE HERE
+    }
+
+    // Copy an element at the back of the queue
+    void enqueue_front(const T &value)
+    {
+        // WRITE YOUR CODE HERE
+    }
+
+    // Remove the element at the back of the queue
+    void dequeue_front()
+    {
+        // WRITE YOUR CODE HERE
+    }
+
+    // Remove all elements from the queue
+    void clear() { this->_size = 0; }
+
+  protected:
+    // Return the index of the element to the front of the queue
+    size_t _tail() const
+    {
+        // WRITE YOUR CODE HERE
+    }
+};
+
+#endif // __queue_enhanced__
---
+++ part-1/radix_sort.hpp
@@ -0,0 +1,31 @@
+#ifndef __radix_sort__
+#define __radix_sort__
+
+#include <algorithm>
+#include <limits>
+
+template <typename I>
+static constexpr int num_digits_v =
+    std::numeric_limits<typename I::value_type>::digits;
+
+template <typename I>
+void radix_sort(const I &begin, const I &end,
+                int digit = num_digits_v<I>)
+{
+    if (begin >= end) return;
+    using T = typename I::value_type;
+    const T mask = static_cast<T>(1) << digit;
+    auto i = begin;
+    auto j = end - 1;
+    while (true) {
+        while (i != end && (*i & mask) == 0) i++;
+        while (j != begin - 1 && (*j & mask) != 0) j--;
+        if (i >= j) break;
+        std::swap(*i, *j);
+    }
+    if (digit == 0) return;
+    radix_sort(begin, i, digit - 1);
+    radix_sort(j + 1, end, digit - 1); // or (i, end, digit-1)
+}
+
+#endif // __radix_sort__
