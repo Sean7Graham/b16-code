e6d2859584eba1e5279de09cb58392f477787695
Sean Graham <141180490+Sean7Graham@users.noreply.github.com>
2024-02-17 11:36:11 +0000

    Q6.2 Finished

--- part-2/binary_search_tree_enhanced.hpp
+++ part-2/binary_search_tree_enhanced.hpp
@@ -5,12 +5,24 @@
 
 template <typename T> T bst_min(const T &tree)
 {
-    // WRITE YOUR CODE HERE
+    // WRITE YOUR CODE HERE (Q6.2)
+
+    if (!tree || !left(tree)) {
+        return tree;
+    } else {
+        return bst_min(left(tree));
+    }
 }
 
 template <typename T> T bst_max(const T &tree)
 {
-    // WRITE YOUR CODE HERE
+    // WRITE YOUR CODE HERE (Q6.2)
+
+    if (!tree || !right(tree)) {
+        return tree;
+    } else {
+        return bst_max(right(tree));
+    }
 }
 
 #endif // __binary_saerch_tree_enhanced__
